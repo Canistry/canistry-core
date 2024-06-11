3c3425a43dfb3daaad44a72488af85d57b9f721e
SoftCysec <softengdedan@gmail.com>
2024-06-11 23:07:43 +0300

    chore: Update vite.config.js to use node:url and node:path modules

--- src/canistry_frontend/vite.config.js
+++ src/canistry_frontend/vite.config.js
@@ -1,43 +1,42 @@
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
 import dotenv from 'dotenv';
+import path from 'node:path';
 
 dotenv.config({ path: '../../.env' });
 
 export default defineConfig({
-  build: {
-    emptyOutDir: true,
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
+    build: {
+        emptyOutDir: true,
     },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:4943",
-        changeOrigin: true,
-      },
+    optimizeDeps: {
+        esbuildOptions: {
+            define: {
+                global: "globalThis",
+            },
+        },
     },
-  },
-  plugins: [
-    react(),
-    environment("all", { prefix: "CANISTER_" }),
-    environment("all", { prefix: "DFX_" }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: "declarations",
-        replacement: fileURLToPath(
-          new URL("../declarations", import.meta.url)
-        ),
-      },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:4943",
+                changeOrigin: true,
+            },
+        },
+    },
+    plugins: [
+        react(),
+        environment("all", { prefix: "CANISTER_" }),
+        environment("all", { prefix: "DFX_" }),
     ],
-  },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src',
+                import.meta.url)),
+            'dfx-generated': path.resolve(__dirname, './src/declarations'),
+            'declarations': path.resolve(__dirname, './src/declarations'),
+        },
+    },
 });
